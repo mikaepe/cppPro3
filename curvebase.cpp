0131c978c01cabdf67e4a2bd355c77d29d6b6e5b
Hanna Hultin <hannahultin@live.se>
2016-11-15 15:16:22 +0100

    started writing up functions and classes

--- curvebase.cpp
+++ curvebase.cpp
@@ -20,16 +20,34 @@ public:
 
 };
 
-// use integration from project 1
+// use integration from project 1 to do arc length integration (see p 10 inheritance slides)
 double Curvebase::integrate(double a, double b){
     return 0;
 }
 
 // do arc length parametrization
+// compute p by Newtons method (see lecture 06) and return xp(p)
 double Curvebase::x(double s){
     return 0;
 }
 
+// do arc length parametrization
+// compute p by Newtons method (see lecture 06) and return yp(p)
 double Curvebase::y(double s){
     return 0;
 }
+
+class xLine: public Curvebase{
+    // konstruktor: start-x, slut-x, konstant-y
+    // overwrite integrate, xp, yp, dxp, dyp, x(s), y(s)
+};
+
+class yLine: public Curvebase{
+    // konstruktor: start-y, slut-y, konstant-x
+    // overwrite integrate, xp, yp, dxp, dyp, x(s), y(s)
+};
+
+class fCurve: public Curvebase{
+    // konstruktor: start-x, skift-x, slut-z
+    // overwrite xp, yp, dxp, dyp
+};
