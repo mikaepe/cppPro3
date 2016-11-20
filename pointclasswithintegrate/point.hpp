e6f7cfc21d0a5fa20c934c62d2effaa876a972fa
Mikael Persson <mikaepe@kth.se>
2016-11-20 18:44:05 +0100

    newton solver implemented in point class for demonstration

--- pointclasswithintegrate/point.hpp
+++ pointclasswithintegrate/point.hpp
@@ -1,14 +1,16 @@
-/* testing classes in c++
+/* testing:
+ * a basic class with ASI-integral and Newtonsolver
  */
 
 #ifndef POINT_HPP
 #define POINT_HPP
 
 #include "integrate.cpp"
+#include "newtsolve.cpp"
 
 class Point{
   private:
-    double x;			 	// Data members
+    double x;
     double y;
   public:
     Point(double xx = 0.0, double yy = 0.0) : x(xx), y(yy) {}
@@ -24,6 +26,18 @@ class Point{
       return t*t;
     }
 
+    // FUNCTION FOR TESTING NEWTONSOLVER
+    static double fquad(double t)
+    {
+      return (t-1.0)*(t-3.0);
+    }
+
+    // DERIVATIVE FOR TESTING NEWTONSOLVER
+    static double dfquad(double t)
+    {
+      return 2.0*t - 4.0;
+    }
+
     // TESTA OM ASI FRÅN INTEGRATE FUNKAR:
     double integr8(double a,double b){
       double res, tol;
@@ -32,7 +46,11 @@ class Point{
       return res;
     }
 
-    // OPERATOR OVERLOADING
+    double solve(double guess) {
+      double res, tol = 1e-6;
+      res = newtsolve(guess,tol,&fquad,&dfquad);
+      return res;
+    }
 
 };
 
--- pointclasswithintegrate/test.cpp
+++ pointclasswithintegrate/test.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 #include "point.hpp"
 
+//#include "newtsolve.cpp"
+
+double fquad(double x){
+  return (x-1.0)*(x-3.0);
+}
+
+double dfquad(double x){
+  return 2.0*x - 4.0;
+}
+
+
 using namespace std;
 int main(void){
 
@@ -14,7 +25,25 @@ int main(void){
 
   double I;
   I = p.integr8(0.0,2.0);
-  cout << "integr8 = " << I << endl;
+  cout << "integr8(0,2) = " << I << endl;
+  I = p.integr8(0,1);
+  cout << "integr8(0,1) = " << I << endl;
+
+  /*
+  double x1;
+  x1 = newtsolve(1.123,1e-4,&fquad,&dfquad);
+  cout << "root1 = " << x1 << endl;
+  double x2;
+  x2 = newtsolve(2.91,1e-4,&fquad,&dfquad);
+  cout << "root2 = " << x2 << endl;
+  */
+
+  double root;
+  root = p.solve(1.333);
+  cout << "root1 = " << root << endl;
+  root = p.solve(2.3333);
+  cout << "root2 = " << root << endl;
+
 
   return 0;
 }
