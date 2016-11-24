1d824f614c831b5dd5d5ef2c280069a8215ef169
Hanna Hultin <hannahultin@live.se>
2016-11-24 15:27:04 +0100

    added and deleted files

--- xLine.cpp
+++
@@ -1,4 +0,0 @@
-//
-// Created by Hanna on 2016-11-24.
-//
-
--- xLine.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Hanna on 2016-11-24.
-//
-
-#ifndef CPPPRO3_XLINE_H
-#define CPPPRO3_XLINE_H
-
-#endif //CPPPRO3_XLINE_H
