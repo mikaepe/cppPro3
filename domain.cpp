57b9c7b34bfba0f91953065407a8e658f8333c10
Mikael Persson <mikaepe@kth.se>
2016-11-29 16:51:33 +0100

    comments

--- domain.cpp
+++ domain.cpp
@@ -6,8 +6,6 @@
 
 /*
  * .cpp-file for class domain. See also domain.hpp.
- * Constructor: Input is 4 curvebase objects
- * 		Setting m_ = n_ = 0 and 
  */
 
 
@@ -18,80 +16,69 @@ Domain::Domain(Curvebase& s1, Curvebase& s2, Curvebase& s3, Curvebase& s4) {
   sides[2] = &s3;
   sides[3] = &s4;
 
-  m_ = n_ = 0;
-  x_ = y_ = NULL;
-  xLo = xRi = xTo = xLe = NULL;
-  yLo = yRi = yTo = yLe = NULL;
+  m_ = n_ = 0;				// number of grid points
+  x_ = y_ = NULL;			// arrays for grid coordinates
 }
 
 // DESTRUCTOR
-Domain::~Domain() {
-  if (m_ > 0) {
-    delete [] x_;
+Domain::~Domain() {		
+  if (m_ > 0) {		// Could as well check if n_>0, since both
+    delete [] x_;	// need to be positive to generate the grid
     delete [] y_;
-    
-    delete [] xLo;
-    delete [] xRi;
-    delete [] xTo;
-    delete [] xLe;
-
-    delete [] yLo;
-    delete [] yRi;
-    delete [] yTo;
-    delete [] yLe;
   }
 }
 
-// MEMBER FUNCTIONS
-double Domain::phi1(double t) { return t; }
-double Domain::phi2(double t) { return 1.0-t; }
 
+
+// MEMBER FUNCTIONS ---------------------------------------------------
+
+// Linear interpolation functions
+double Domain::phi1(double t) { 
+  return t; 				// phi1(0) = 0, phi1(1) = 1
+}
+double Domain::phi2(double t) { 
+  return 1.0-t; 			// phi2(0) = 1, phi2(1) = 0
+}
+
+// Generates the grid and sets it to 
 void Domain::grid_generation(int n, int m) {
   if ((n < 1) || (m < 1)) {
+    // Need n and m > 0 to generate any grid. Else:
     std::cout << "Warning: Non positive grid size." << std::endl; 
-    // TODO fixa nån bättre felhantering
     std::cout << "No grid generated" << std::endl;
-    
-    return;
+    return; 				// No grid is generated
   }
-  std::cout << "hej???" << std::endl;
-  						// TODO kolla att x1(0) = x4(0) osv..
+  					// TODO kolla att x1(0) = x4(0) osv..
 
-  if (n != 0) {					// or m != 0
+  if (n != 0) {				// Reset the arrays
     delete[] x_; 
     delete[] y_;
-
-    delete[] xLo;
-    delete[] xRi;
-    delete[] xTo;
-    delete[] xLe;
-
-    delete[] yLo;
-    delete[] yRi;
-    delete[] yTo;
-    delete[] yLe;
   }
 
   n_ = n; 
   m_ = m;
 
-  // INSTEAD OF COMPUTING x_  (n+1)*(m+1) times, compute
-  // xLo, xRi, xTo, xLe once only.
+  /* The sides' coordinates are computed once only, i.e. there is
+   * 4*(n+1)+4*(m+1) calls to x(s) and y(s). If instead, one would
+   * call x(s) and y(s) for each of the grid points there would be
+   * 16*(n+1)*(m+1) calls. Consider memory if n,m are large.
+   */
 
-  xLo = new double[n_+1];
-  xRi = new double[m_+1];
-  xTo = new double[n_+1];
-  xLe = new double[m_+1];
+  double *xLo,*xRi,*xTo,*xLe,*yLo,*yRi,*yTo,*yLe; 
 
-  yLo = new double[n_+1];
+  xLo = new double[n_+1];	// Lower boundary x-coords
+  xRi = new double[m_+1];	// Right boundary
+  xTo = new double[n_+1];	// Top boundary
+  xLe = new double[m_+1];	// Left boundary
+
+  yLo = new double[n_+1];	// same for the y-coords
   yRi = new double[m_+1];
   yTo = new double[n_+1];
   yLe = new double[m_+1];
 
+  double h1= 1.0/n; double h2 = 1.0/m;	// Step sizes
 
-  double h1= 1.0/n; double h2 = 1.0/m;
-
-  for (int i=0; i <= n_; i++) {
+  for (int i=0; i <= n_; i++) {		// Loop the normalized coordinate for x
     xLo[i] = sides[0]->x(i*h1);
     xTo[i] = sides[2]->x(i*h1);
     
@@ -99,7 +86,7 @@ void Domain::grid_generation(int n, int m) {
     yTo[i] = sides[2]->y(i*h1);
 
   }
-  for (int j=0; j <= m_; j++) {
+  for (int j=0; j <= m_; j++) {		// Loop the normalized coordinate for y
     xRi[j] = sides[1]->x(j*h2);
     xLe[j] = sides[3]->x(j*h2);
     
@@ -107,45 +94,53 @@ void Domain::grid_generation(int n, int m) {
     yLe[j] = sides[3]->y(j*h2);
   }
 
-
-  x_ = new double[(n_+1)*(m_+1)];
-  y_ = new double[(n_+1)*(m_+1)];
-
-
+  x_ = new double[(n_+1)*(m_+1)];	// x-coordinates for the entire grid
+  y_ = new double[(n_+1)*(m_+1)];	// y-coordinates for the same
 
   for (int i = 0; i <= n_; i++) {
-
     for (int j = 0; j <= m_; j++) {
 
       x_[j+i*(m_+1)] = 
-	phi2(i*h1)*xLe[j] //sides[3]->x(j*h2) 		// left side
-	+ phi1(i*h1)*xRi[j] //sides[1]->x(j*h2) 		// right side
-	+ phi2(j*h2)*xLo[i] //sides[0]->x(i*h1)		// bottom side
-	+ phi1(j*h2)*xTo[i] //sides[2]->x(i*h1)		// top side
-	- phi2(i*h1)*phi2(j*h2)*xLo[0] //sides[0]->x(0)	// lower left 
-	- phi1(i*h1)*phi2(j*h2)*xLo[n_] //sides[0]->x(1)	// lower right
-	- phi2(i*h1)*phi1(j*h2)*xTo[0] //sides[2]->x(0)	// top left
-	- phi1(i*h1)*phi1(j*h2)*xTo[n_]; //sides[2]->x(1);	// top right
+	phi2(i*h1)*xLe[j] 		// left side
+	+ phi1(i*h1)*xRi[j] 		// right side
+	+ phi2(j*h2)*xLo[i] 		// bottom side
+	+ phi1(j*h2)*xTo[i] 		// top side
+	- phi2(i*h1)*phi2(j*h2)*xLo[0] 	// lower left 
+	- phi1(i*h1)*phi2(j*h2)*xLo[n_] // lower right
+	- phi2(i*h1)*phi1(j*h2)*xTo[0] 	// top left
+	- phi1(i*h1)*phi1(j*h2)*xTo[n_];// top right
 	
       y_[j+i*(m_+1)] = 
-	phi2(i*h1)*yLe[j] //sides[3]->y(j*h2) 		// left side
-	+ phi1(i*h1)*yRi[j] //sides[1]->y(j*h2) 		// right side
-	+ phi2(j*h2)*yLo[i] //sides[0]->y(i*h1)		// bottom side
-	+ phi1(j*h2)*yTo[i] // sides[2]->y(i*h1)		// top side
-	- phi2(i*h1)*phi2(j*h2)*yLo[0] //sides[0]->y(0)	// fixa x(00)
-	- phi1(i*h1)*phi2(j*h2)*yLo[n_] //sides[0]->y(1)	// fixa
-	- phi2(i*h1)*phi1(j*h2)*yTo[0] //sides[2]->y(0)	// fixa
-	- phi1(i*h1)*phi1(j*h2)*yTo[n_];// sides[2]->y(1);	// fixa
+	phi2(i*h1)*yLe[j] 		// equivalent to x above
+	+ phi1(i*h1)*yRi[j]
+	+ phi2(j*h2)*yLo[i]
+	+ phi1(j*h2)*yTo[i]
+	- phi2(i*h1)*phi2(j*h2)*yLo[0]
+	- phi1(i*h1)*phi2(j*h2)*yLo[n_]
+	- phi2(i*h1)*phi1(j*h2)*yTo[0]
+	- phi1(i*h1)*phi1(j*h2)*yTo[n_];
     }
-  }
+  }	
+
+  delete[] xLo;
+  delete[] xRi;
+  delete[] xTo;
+  delete[] xLe;
+
+  delete[] yLo;
+  delete[] yRi;
+  delete[] yTo;
+  delete[] yLe; 
 }
 
-void Domain::print() {
+// Print (for testing) the grid coordinates: Careful if n,m are large.
+void Domain::print() {	
   for (int i = 0; i < (n_+1)*(m_+1); i++) {
     std::cout << "[" << x_[i] << "," << y_[i] << "]" << std::endl;
   }
-}
+}	
 
+// Write the grid to an external file to enable visualization in e.g. matlab.
 void Domain::writeFile(){
   if (n_ < 1 || m_ < 1) {
     std::cout << "No grid available for writeFile()" << std::endl;
--- domain.hpp
+++ domain.hpp
@@ -5,17 +5,14 @@
 
 class Domain {
   private:
-    Curvebase * sides[4];
-    int m_, n_;
-    double *x_,*y_;	
-    double *xLo,*xRi,*xTo,*xLe;
-    double *yLo,*yRi,*yTo,*yLe;
+    Curvebase * sides[4];		// Pointers to curves of the 4 sides
+    int m_, n_;				// # of grid points in x and y
+    double *x_,*y_;			// Arrays for coordinates in grid
 
-    // Linear interpolation functions
-    double phi1(double t);
-    double phi2(double t);
+    double phi1(double t);		// Linear interpolation functions
+    double phi2(double t);		
+
   public:
-
     // CONSTRUCTOR
     Domain(Curvebase& s1, Curvebase& s2, Curvebase& s3, Curvebase& s4);
 
@@ -23,9 +20,9 @@ class Domain {
     ~Domain();
 
     // FUNCTIONS
-    void grid_generation(int n, int m);
-    void print();
-    void writeFile();
+    void grid_generation(int n, int m);	// Generates the grid (x_ and y_)
+    void print();			// Print points of grid to console
+    void writeFile();			// Write points to .bin-file (use matlab to view)
 };
 
 #endif //DOMAIN_HPP
